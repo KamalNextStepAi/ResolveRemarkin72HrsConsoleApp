18e9feb918a42fd118a31252cb9f085d8b0dd20a
kamal.pandey@nextsteps.ai <kamal.pandey@nextsteps.ai>
2021-09-09 11:04:28 +0530

    Mentor Chat - if resolved remarks are sent by mentor, no response till 72hours, it should get resolved

---
+++ ResolveRemarkByMentor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolveRemarkByMentor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con;
+            try
+            {
+                
+                    con = new SqlConnection(Properties.Settings.Default.ConnectionString);
+
+                    con.Open();
+                    SqlCommand sql_cmnd = new SqlCommand("EXEC [mentor].[ResolveRemarkByMentor]", con);
+                    sql_cmnd.ExecuteNonQuery();
+                    con.Close();
+               
+            }
+            catch (Exception ex)
+            {
+                //throw ex;
+                Console.ForegroundColor
+                     = ConsoleColor.Red;
+
+                Console.WriteLine("process cancelled due to this Error:"+ ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
